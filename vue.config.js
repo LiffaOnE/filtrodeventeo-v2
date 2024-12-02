2dd3b5df5807ff7faada6dc1d623db48615745f8
Felipe <felipe.m.fuentes67@gmail.com>
2024-12-02 12:51:19 -0300

    dist v2

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/filtrodeventeo-v2/' // Reemplaza con el nombre de tu repositorio
+    : '/'
+};
+
+
