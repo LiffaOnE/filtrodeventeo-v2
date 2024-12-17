a6b7aa5f2bf9b7240f5509bf9e76ba362cab30a9
Felipe <felipe.m.fuentes67@gmail.com>
2024-12-17 05:46:41 -0300

    Updates

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-// const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/filtrodeventeo-v2/' // Reemplaza con el nombre de tu repositorio
-    : '/'
-};
-
-
