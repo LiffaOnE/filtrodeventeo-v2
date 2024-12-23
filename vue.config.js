4705b9b8693c0e5587956436300f76e9b1b059e6
Felipe <felipe.m.fuentes67@gmail.com>
2024-12-23 08:43:37 -0300

    testing parte mill

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 // const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/filtrodeventeo-v2/' // Reemplaza con el nombre de tu repositorio
+    ? '/' // Reemplaza con el nombre de tu repositorio
     : '/'
 };
 
